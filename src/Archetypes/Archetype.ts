57f58b1344ccbf553c24bcc28783b6fbd6090c87
Willian Gonçalves <willian.p.g@outlook.com>
2023-11-29 19:13:39 -0300

    Requisito 04 - Crie a classe Archetype

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,33 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  public _name: string;
+  public _special: number;
+  public _cost: number;
+
+  constructor(name: string, special: number, cost: number) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  public static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;
